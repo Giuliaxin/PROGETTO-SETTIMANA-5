69acc0b220a33e86b7aa145ea8916f8937877927
Giuliaxin <lavpetricore@gmail.com>
2026-06-06 01:41:08 +0200

    nav bar sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 //
 // Aggiungi qui il codice JavaScript se serve (es. modale, scroll handler).
 // Per la Versione Base spesso non serve niente: tutto si fa in CSS.
+
+
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('site-header');
+    
+    if (window.scrollY > 100) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
